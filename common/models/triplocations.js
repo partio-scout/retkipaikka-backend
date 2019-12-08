9674ad2aee7b6153c671995c9c12b8ed3deea192
Jani Niittymäki <jani.niitty@outlook.com>
2019-12-08 16:56:17 +0200

    added acls to models

--- common/models/triplocations.js
+++ common/models/triplocations.js
@@ -106,6 +106,7 @@ module.exports = function (Triplocations) {
         delete newObject["filters"];
         const uuid = uuidv4();
         newObject["location_id"] = uuid;
+        newObject["location_accepted"] = false;
 
 
         await Triplocations.create(newObject).then(res => {
@@ -202,7 +203,7 @@ module.exports = function (Triplocations) {
 
     Triplocations.remoteMethod(
         'fetchLocations', {
-        http: { path: '/fetchlocations', verb: 'get' },
+        http: { path: '/fetchLocations', verb: 'get' },
         accepts: [
             { arg: 'filter', type: 'object', http: { source: 'query' } },
             { arg: 'req', type: 'object', http: { source: 'req' } },
